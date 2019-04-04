c0f0c67b55acd81e276b2e5c4ae6e7f0bc5f2271
Matthew <mf9646a@alumni.american.edu>
2019-04-04 08:19:51 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School# code here!
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+  sorted = {}
+  roster.each do |grade, students|
+    sorted[grade] = students.sort
+  end
+  sorted
+end
+end
+
+ #
+ # If the roster already has a key of grade 9, we'll be able to push AC Slater into the array that the grade 9 key points to.
+ #
+ # Otherwise, we'll need to first create the key of grade 9 and point it to an empty array.
+ #
+ # However, if we're in the scenario in which the grade 9 key already exists, we will erase its current contents by creating a key of 9 and setting it equal to an empty array!
